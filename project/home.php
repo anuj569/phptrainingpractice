47913480a804665f53c0577ef94a89841b8d9ed8
anujkumar pandey <anuj569@gmail.com>
2021-08-09 11:28:32 +0530

    session

--- project/home.php
+++ project/home.php
@@ -3,6 +3,7 @@
 <div class="container">
   <h3 align="center">
     <?php
+    //$_SESSION['cart'] = array();
     $user = "Guest";
     if(!empty($_SESSION['name'])){
         $user = $_SESSION['name'];
@@ -10,12 +11,16 @@
     echo "Welcome $user";
     ?>
     <?php 
-      print "<PRE>";
-      print_r($_GET);
-      print_r($_POST);
-      print_r($_REQUEST);
+      //print "<PRE>";
+      //print_r($_GET);
+      //print_r($_POST);
+      //print_r($_REQUEST);
+      print_r($_SESSION);
     ?>
   </h3>  
 </div>
+<a href="home_new.php">Add 1 more product</a>
+<br/>
+<a href="logout.php">Logout</a>
 </body>
 </html>
